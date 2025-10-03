22e7c34b0ebff9387bd9f5de9d5b7d1269710711
skygge <coelhodd.pro@gmail.com>
2025-10-03 11:48:38 -0300

    created a model for the note object

---
+++ models/note.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+class Note:
+    # Everytime a Note object is created the method below is called
+    def __init__(self, title, content, name, context, date_created=None):
+        self.title = title
+        self.content = content
+        self.name = name
+        self.context = context
+        self.date_created = date_created or str(date.today())
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "content": self.content,
+            "name": self.name
+            "date": self.date_created
+            "context": self.context
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            title=data["title"],
+            content=data["content"],
+            name=data["name"],
+            context=data["context"],
+            date_created=data["date"]
+        )
